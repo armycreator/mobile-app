7b0a03f1865dc9f4441e2f05369a37de6178eeee
Julien Deniau <julien.deniau@gmail.com>
2017-03-30 14:08:08 +0200

    add army list menu

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -4,7 +4,7 @@ import { View, TouchableHighlight, Text } from 'react-native';
 import NavigationBar from 'react-native-navbar';
 import colors from '../colors';
 
-function Menu({ user, onLogout }) {
+function Menu({ user, onLogout, onArmyList }) {
   return (<View>
     <NavigationBar
       title={{ title: 'Menu', tintColor: colors.white }}
@@ -12,12 +12,22 @@ function Menu({ user, onLogout }) {
     />
 
     {user &&
-      <TouchableHighlight
-        onPress={onLogout}
-        style={{ backgroundColor: 'red', padding: 30, marginTop: 30 }}
-      >
-        <Text>Logout</Text>
-      </TouchableHighlight>
+      <View>
+        <TouchableHighlight
+          onPress={onArmyList}
+          style={{ padding: 30, marginTop: 30 }}
+        >
+          <Text>Mes listes</Text>
+        </TouchableHighlight>
+
+
+        <TouchableHighlight
+          onPress={onLogout}
+          style={{ backgroundColor: 'red', padding: 30, marginTop: 30 }}
+        >
+          <Text>Logout</Text>
+        </TouchableHighlight>
+      </View>
     }
   </View>);
 };
