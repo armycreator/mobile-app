aa4e500705f6039dc80fe95fc38b0927762a6eb8
Julien Deniau <julien.deniau@gmail.com>
2017-04-04 23:22:30 +0200

    move title to routes

--- src/App.js
+++ src/App.js
@@ -2,41 +2,21 @@
 
 import React, { Component } from 'react';
 import { Text, TouchableHighlight, StatusBar, View } from 'react-native';
-import { composeWithDevTools } from 'remote-redux-devtools';
-import { applyMiddleware, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import createHistory from 'history/createMemoryHistory';
-import { ConnectedRouter, routerMiddleware, push } from 'react-router-redux';
-
+import { ConnectedRouter, push } from 'react-router-redux';
 import { Map } from 'immutable'
-import NavigationBar from 'react-native-navbar';
 import SideMenu from 'react-native-side-menu';
+import createHistory from 'history/createMemoryHistory';
 import styled from 'styled-components/native';
 import Menu from './component/Menu';
-import HamburgerMenu from './component/HamburgerMenu';
 import sdk from './Sdk';
 import colors from './colors';
-import reducer from './reducer';
 import Routes from './routes';
+import configureStore from './configureStore';
 
 // global.XMLHttpRequest = global.originalXMLHttpRequest || global.XMLHttpRequest;
 const history = createHistory();
-const composeEnhancers = composeWithDevTools({
-  // Specify here name, actionsBlacklist, actionsCreators and other options
-});
-
-const middlewares = [
-  thunk,
-  routerMiddleware(history)
-];
-
-const store = createStore(
-  reducer,
-  composeEnhancers(
-    applyMiddleware(...middlewares)
-  )
-);
+const store = configureStore(history);
 
 const Container = styled.View`
   flex: 1;
@@ -45,10 +25,8 @@ const Container = styled.View`
 
 export default class App extends Component {
   state: {
-    user: ?Map<any, any>,
     title: string,
     isMenuOpen: boolean,
-    army: ?Map<any, any>,
   };
 
 
@@ -59,10 +37,8 @@ export default class App extends Component {
     (this: any).toggleMenu = this.toggleMenu.bind(this);
 
     this.state = {
-      user: null,
       title: 'Armycreator',
       isMenuOpen: false,
-      army: null,
     };
   }
 
@@ -71,17 +47,16 @@ export default class App extends Component {
   }
 
   handleLogout() {
-    this.setState({ title: 'Connexion', user: null, isMenuOpen: false });
+    this.setState({ isMenuOpen: false });
     store.dispatch(push('/login'));
   }
 
   render() {
-    const { army, title } = this.state;
+    const { title } = this.state;
 
     const menu = <Menu
       onLogout={this.handleLogout}
       onArmyList={() => null}
-      user={this.state.user}
     />;
 
 
@@ -96,17 +71,7 @@ export default class App extends Component {
             >
               <StatusBar hidden />
 
-              <NavigationBar
-                title={{ title, tintColor: colors.white }}
-                tintColor={colors.secondary}
-                leftButton={<HamburgerMenu
-                  onPress={() => this.setState({ isMenuOpen: true })}
-                />}
-              />
-
-              <Container>
-                <Routes history={history} />
-              </Container>
+              <Routes history={history} />
             </SideMenu>
           </Container>
         </ConnectedRouter>
--- src/routes.js
+++ src/routes.js
@@ -1,41 +1,89 @@
 import React from 'react';
+import { Text, View } from 'react-native';
 import { AndroidBackButton, Router, Route, Switch } from 'react-router-native';
 import { ConnectedRouter } from 'react-router-redux';
+import NavigationBar from 'react-native-navbar';
+import styled from 'styled-components/native';
+import HamburgerMenu from './component/HamburgerMenu';
 import Layout from './component/Layout';
 import ArmyList from './component/ArmyList';
 import Army from './component/Army';
 import Login from './component/login';
+import colors from './colors';
 
 const routes = [
   {
     path: '/armies/:id',
     exact: true,
-    main: ({ location }) => <Army army={location.state.army} />
+    main: ({ location }) => <Army army={location.state.army} />,
+    title: ({ location }) => {
+      return <NavigationBarText>{location.state.army.get('name')}</NavigationBarText>;
+    },
   },
   {
     path: '/armies/',
     exact: true,
-    main: () => <ArmyList />
+    main: () => <ArmyList />,
+    title: () => <NavigationBarText>Mes dernières listes</NavigationBarText>,
   },
   {
-    main: () => <Login />
+    main: () => <Login />,
+    title: () => <NavigationBarText>Connexion</NavigationBarText>,
   },
 ];
+
+const NavigationBarText = styled.Text`
+  font-size: 17;
+  letter-spacing: 0.5;
+  color: ${colors.white};
+`;
+
+function NavigationBarTitle() {
+  const title = <Switch>
+    {routes.map((route, index) =>
+      <Route
+        key={index}
+        path={route.path}
+        exact={route.exact}
+        component={route.title}
+      />
+    )}
+  </Switch>;
+
+  return (
+    <NavigationBar
+      title={title}
+      tintColor={colors.secondary}
+      leftButton={<HamburgerMenu
+        onPress={() => this.setState({ isMenuOpen: true })}
+      />}
+    />
+  );
+}
+
+const Container = styled.View`
+  flex: 1;
+  background-color: ${colors.background};
+`;
 
 export default function Routes({ history }) {
   return (<ConnectedRouter history={history}>
     <AndroidBackButton>
       <Layout>
-        <Switch>
-          {routes.map((route, index) =>
-            <Route
-              key={index}
-              path={route.path}
-              exact={route.exact}
-              component={route.main}
-            />
-          )}
-        </Switch>
+        <Container>
+          <NavigationBarTitle />
+
+          <Switch>
+            {routes.map((route, index) =>
+              <Route
+                key={index}
+                path={route.path}
+                exact={route.exact}
+                component={route.main}
+              />
+            )}
+          </Switch>
+        </Container>
       </Layout>
     </AndroidBackButton>
   </ConnectedRouter>);
